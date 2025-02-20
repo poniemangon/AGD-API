2ae06d0c57664bd5b8704ab1b640d9d9f0818f73
poniemangon <108033301+poniemangon@users.noreply.github.com>
2025-02-20 09:19:04 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,17 @@
 const yahooFinance = require('yahoo-finance2').default;
 const express = require('express');
-const cron = require('node-cron');
-const fs = require('fs');
-const path = require('path');
-const cors = require('cors'); // Importar CORS
+const cors = require('cors');
 
-// Configuración de la aplicación Express
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Habilitar CORS para todas las rutas
-
-app.use(cors({ origin: '*' })); // Permite acceso desde cualquier origen
-
-
-// Ruta para obtener las cotizaciones desde el archivo JSON
-app.get('/quotes', (req, res) => {
-    fs.readFile(path.join(__dirname, 'quotes.json'), 'utf-8', (err, data) => {
-        if (err) {
-            return res.status(500).send({ message: 'Error al leer el archivo JSON' });
-        }
-        res.json(JSON.parse(data));
-    });
-});
+app.use(cors({ origin: '*' }));
 
 // Función que consulta la API de Yahoo Finance
 const fetchYahooFinanceData = async (symbol) => {
     try {
         const quote = await yahooFinance.quote(symbol);
-        
         return {
             symbol,
             price: quote.regularMarketPrice,
@@ -40,20 +23,9 @@ const fetchYahooFinanceData = async (symbol) => {
     }
 };
 
-// Función que guarda las cotizaciones en un archivo JSON
-const saveQuotesToJson = (quotes) => {
-    fs.writeFile(path.join(__dirname, 'quotes.json'), JSON.stringify(quotes, null, 2), (err) => {
-        if (err) {
-            console.error('Error saving quotes:', err);
-        } else {
-            console.log('Cotizaciones guardadas exitosamente');
-        }
-    });
-};
-
-// Función que actualiza las cotizaciones dos veces al día (cada 12 horas)
-const updateQuotes = async () => {
-    const symbols = ['AGD.AX', 'AGLD.V']; // Los símbolos de Austral Gold en Yahoo Finance
+// Ruta para obtener las cotizaciones en tiempo real
+app.get('/quotes', async (req, res) => {
+    const symbols = ['AGD.AX', 'AGLD.V']; // Símbolos de Austral Gold en Yahoo Finance
     
     let quotes = [];
     
@@ -63,16 +35,11 @@ const updateQuotes = async () => {
             quotes.push(quote);
         }
     }
+    
+    res.json(quotes);
+});
 
-    saveQuotesToJson(quotes);
-};
-
-// Programar el scraping para que se ejecute dos veces al día (a las 9:00 AM y 9:00 PM)
-cron.schedule('0 9,21 * * *', updateQuotes); // 9:00 AM y 9:00 PM
-
-// Iniciar la API en el puerto 3000
+// Iniciar la API
 app.listen(PORT, () => {
     console.log(`API corriendo en http://localhost:${PORT}`);
-    // Realizar el primer scraping al iniciar la aplicación
-    updateQuotes();
 });
