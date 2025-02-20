0d729df1ff0d9bb8deb273d6e5cc7b5d9ac850c1
poniemangon <108033301+poniemangon@users.noreply.github.com>
2025-02-20 12:24:54 -0300

    share price information

--- index.js
+++ index.js
@@ -13,10 +13,19 @@ const fetchYahooFinanceData = async (symbol) => {
     try {
         const quote = await yahooFinance.quote(symbol);
         return {
-            symbol,
-            price: quote.regularMarketPrice,
-            percent: quote.regularMarketChangePercent
-        };
+        symbol,
+        price: quote.regularMarketPrice,
+        percent: quote.regularMarketChangePercent,
+        bid: quote.bid,
+        ask: quote.ask,
+        open: quote.regularMarketOpen,
+        high: quote.regularMarketDayHigh,
+        low: quote.regularMarketDayLow,
+        prevClose: quote.regularMarketPreviousClose,
+        volume: quote.regularMarketVolume,
+        fiftyTwoWeekHigh: quote.fiftyTwoWeekHigh,
+        fiftyTwoWeekLow: quote.fiftyTwoWeekLow
+    };
     } catch (error) {
         console.error(`Error fetching data for ${symbol}:`, error);
         return null;
@@ -25,7 +34,22 @@ const fetchYahooFinanceData = async (symbol) => {
 
 // Ruta para obtener las cotizaciones en tiempo real
 app.get('/quotes', async (req, res) => {
-    const symbols = ['AGD.AX', 'AGLD.V', 'AGLDF', 'GC=F', 'SI=F']; // Símbolos de Austral Gold en Yahoo Finance
+    const symbols = ['AGD.AX', 'AGLD.V', 'AGLDF', 'GOLD', 'SILVER'];
+    
+    let quotes = [];
+    
+    for (let symbol of symbols) {
+        const quote = await fetchYahooFinanceData(symbol);
+        if (quote) {
+            quotes.push(quote);
+        }
+    }
+    
+    res.json(quotes);
+});
+
+app.get('/share-price-information', async (req, res) => {
+    const symbols = ['AGD.AX', 'AGLD.V'];
     
     let quotes = [];
     
